d71a36196b33dd3b60fc66e573e1fa16b00d0be6
stephenobie <79488120+stephenobie@users.noreply.github.com>
2021-08-22 12:59:55 -0400

    added footer/header more layouts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 
-import './App.css';
-import { Navbar } from 'react-bootstrap';
-import React from 'react';
-
-
-function App() {
-  return (
-    <div>
-      <header />
-        <Navbar />
-        <projects />
-        <footer />
-
-        
-
-    
-    </div>
-  );
+import React, { Component } from "react";
+import Header from "./components/pages/Header";
+import About from "./components/pages/About";
+import Resume from "./components/pages/Resume";
+import Portfolio from "./components/pages/Portfolio";
+import ContactUs from "./components/pages/Contact";
+import Footer from "./components/pages/Footer";
+import resumeData from "./resumeData";
+class App extends Component {
+render() {
+return (
+<div className="App">
+<Header resumeData={resumeData}/>
+<About resumeData={resumeData}/>
+<Resume resumeData={resumeData}/>
+<Portfolio resumeData={resumeData}/>
+<ContactUs resumeData={resumeData}/>
+<Footer resumeData={resumeData}/>
+</div>
+);
 }
-
+}
 export default App;
