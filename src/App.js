5368243a90443b2fda5dea9faf35aeeff7600a60
stephenobie <79488120+stephenobie@users.noreply.github.com>
2021-08-22 14:43:32 -0400

    got imports working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import React, { Component } from "react";
-import Header from "./components/pages/Header";
-import About from "./components/pages/About";
-import Resume from "./components/pages/Resume";
-import Portfolio from "./components/pages/Portfolio";
-import ContactUs from "./components/pages/Contact";
-import Footer from "./components/pages/Footer";
-import resumeData from "./resumeData";
-class App extends Component {
-render() {
-return (
-<div className="App">
-<Header resumeData={resumeData}/>
-<About resumeData={resumeData}/>
-<Resume resumeData={resumeData}/>
-<Portfolio resumeData={resumeData}/>
-<ContactUs resumeData={resumeData}/>
-<Footer resumeData={resumeData}/>
-</div>
-);
-}
-}
+const App = () => <PortfolioContainer />;
+
 export default App;
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import NavTabs from './pages/NavTabs';
+import Home from './pages/Home';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'Projects') {
+        return <Projects />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+    </div>
+  );
+}
+
